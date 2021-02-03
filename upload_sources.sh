c97816c57b8a49f32d7bac85ac77c4d333876fd5
TheHitMan7 <krtik.vrma@gmail.com>
2021-02-03 21:59:10 +0530

    Update release scipt
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

---
+++ upload_sources.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+#
+##############################################################
+# File name       : upload_sources.sh
+#
+# Description     : Upload pre-built packages
+#
+# Copyright       : Copyright (C) 2018-2021 TheHitMan7
+#
+# License         : SPDX-License-Identifier: GPL-3.0-or-later
+##############################################################
+# The BiTGApps scripts are free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# These scripts are distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+##############################################################
+
+# Set defaults
+export ARMEABI="$1"
+export AARCH64="$1"
+export COMMON="$1"
+export CONFIG="$1"
+export APK="$1"
+
+# Server credentials
+set_credentials() {
+  if [ "$SERVER" == "ga" ]; then
+    read -p "Enter user: " user
+    read -p "Enter pass: " pass
+    read -p "Enter host: " host
+  fi
+  if [ "$SERVER" == "dh" ] || [ "$SERVER" == "sf" ]; then
+    read -p "Enter user: " user
+    read -p "Enter host: " host
+  fi
+}
+
+check_credentials() {
+  if [ ! -n "$user" ]; then
+    echo "! No credentials found. Aborting..."
+    exit 1
+  fi
+}
+
+# ARMEABI Sources
+arm_sources() {
+  if [ "$ARMEABI" == "arm" ]; then
+    if [ ! -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "ga" ]; then
+        if [ -n "$COMMONGAPPSRELEASE" ]; then
+          curl -T out/arm/BiTGApps-arm-11.0.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm/R/BiTGApps-arm-11.0.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm/BiTGApps-arm-10.0.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm/Q/BiTGApps-arm-10.0.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm/BiTGApps-arm-9.0.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm/Pie/BiTGApps-arm-9.0.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm/BiTGApps-arm-8.1.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm/Oreo/BiTGApps-arm-8.1.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm/BiTGApps-arm-8.0.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm/Oreo/BiTGApps-arm-8.0.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm/BiTGApps-arm-7.1.2-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm/Nougat/BiTGApps-arm-7.1.2-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm/BiTGApps-arm-7.1.1-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm/Nougat/BiTGApps-arm-7.1.1-${COMMONGAPPSRELEASE}_signed.zip"
+        fi
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          curl -T out/arm/BiTGApps-addon-arm-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/config/arm/BiTGApps-addon-arm-${COMMONADDONRELEASE}_signed.zip"
+        fi
+      fi
+      if [ "$SERVER" == "dh" ]; then
+        if [ -n "$COMMONGAPPSRELEASE" ]; then
+          scp out/arm/BiTGApps-arm-11.0.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm/R
+          scp out/arm/BiTGApps-arm-10.0.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm/Q
+          scp out/arm/BiTGApps-arm-9.0.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm/Pie
+          scp out/arm/BiTGApps-arm-8.1.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm/Oreo
+          scp out/arm/BiTGApps-arm-8.0.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm/Oreo
+          scp out/arm/BiTGApps-arm-7.1.2-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm/Nougat
+          scp out/arm/BiTGApps-arm-7.1.1-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm/Nougat
+        fi
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          scp out/arm/BiTGApps-addon-arm-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/config/arm
+        fi
+      fi
+    fi
+    if [ -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "sf" ]; then
+        if [ -n "$COMMONGAPPSRELEASE" ]; then
+          scp out/arm/BiTGApps-arm-11.0.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm/30
+          scp out/arm/BiTGApps-arm-10.0.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm/29
+          scp out/arm/BiTGApps-arm-9.0.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm/28
+          scp out/arm/BiTGApps-arm-8.1.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm/27
+          scp out/arm/BiTGApps-arm-8.0.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm/26
+          scp out/arm/BiTGApps-arm-7.1.2-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm/25
+          scp out/arm/BiTGApps-arm-7.1.1-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm/25
+        fi
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          scp out/arm/BiTGApps-addon-arm-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/config/arm
+        fi
+      fi
+    fi
+  fi
+}
+
+# AARCH64 Sources
+arm64_sources() {
+  if [ "$AARCH64" == "arm64" ]; then
+    if [ ! -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "ga" ]; then
+        if [ -n "$COMMONGAPPSRELEASE" ]; then
+          curl -T out/arm64/BiTGApps-arm64-11.0.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm64/R/BiTGApps-arm64-11.0.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm64/BiTGApps-arm64-10.0.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm64/Q/BiTGApps-arm64-10.0.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm64/BiTGApps-arm64-9.0.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm64/Pie/BiTGApps-arm64-9.0.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm64/BiTGApps-arm64-8.1.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm64/Oreo/BiTGApps-arm64-8.1.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm64/BiTGApps-arm64-8.0.0-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm64/Oreo/BiTGApps-arm64-8.0.0-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm64/BiTGApps-arm64-7.1.2-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm64/Nougat/BiTGApps-arm64-7.1.2-${COMMONGAPPSRELEASE}_signed.zip"
+          curl -T out/arm64/BiTGApps-arm64-7.1.1-${COMMONGAPPSRELEASE}_signed.zip "ftp://${user}:${password}@${host}/arm64/Nougat/BiTGApps-arm64-7.1.1-${COMMONGAPPSRELEASE}_signed.zip"
+        fi
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          curl -T out/arm64/BiTGApps-addon-arm64-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/config/arm64/BiTGApps-addon-arm64-${COMMONADDONRELEASE}_signed.zip"
+        fi
+      fi
+      if [ "$SERVER" == "dh" ]; then
+        if [ -n "$COMMONGAPPSRELEASE" ]; then
+          scp out/arm64/BiTGApps-arm64-11.0.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm64/R
+          scp out/arm64/BiTGApps-arm64-10.0.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm64/Q
+          scp out/arm64/BiTGApps-arm64-9.0.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm64/Pie
+          scp out/arm64/BiTGApps-arm64-8.1.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm64/Oreo
+          scp out/arm64/BiTGApps-arm64-8.0.0-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm64/Oreo
+          scp out/arm64/BiTGApps-arm64-7.1.2-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm64/Nougat
+          scp out/arm64/BiTGApps-arm64-7.1.1-${COMMONGAPPSRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/arm64/Nougat
+        fi
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          scp out/arm64/BiTGApps-addon-arm64-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/config/arm64
+        fi
+      fi
+    fi
+    if [ -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "sf" ]; then
+        if [ -n "$COMMONGAPPSRELEASE" ]; then
+          scp out/arm64/BiTGApps-arm64-11.0.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm64/30
+          scp out/arm64/BiTGApps-arm64-10.0.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm64/29
+          scp out/arm64/BiTGApps-arm64-9.0.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm64/28
+          scp out/arm64/BiTGApps-arm64-8.1.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm64/27
+          scp out/arm64/BiTGApps-arm64-8.0.0-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm64/26
+          scp out/arm64/BiTGApps-arm64-7.1.2-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm64/25
+          scp out/arm64/BiTGApps-arm64-7.1.1-${COMMONGAPPSRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/arm64/25
+        fi
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          scp out/arm64/BiTGApps-addon-arm64-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/config/arm64
+        fi
+      fi
+    fi
+  fi
+}
+
+# COMMON Sources; Contains both ARMEABI & AARCH64 packages
+common_sources() {
+  if [ "$COMMON" == "common" ]; then
+    if [ ! -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "ga" ]; then
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          curl -T out/common/BiTGApps-addon-assistant-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-assistant-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-calculator-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-calculator-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-calendar-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-calendar-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-contacts-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-contacts-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-deskclock-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-deskclock-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-dialer-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-dialer-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-gboard-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-gboard-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-markup-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-markup-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-messages-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-messages-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-photos-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-photos-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-soundpicker-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-soundpicker-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-vanced-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-vanced-${COMMONADDONRELEASE}_signed.zip"
+          curl -T out/common/BiTGApps-addon-wellbeing-${COMMONADDONRELEASE}_signed.zip "ftp://${user}:${password}@${host}/addon/non-config/BiTGApps-addon-wellbeing-${COMMONADDONRELEASE}_signed.zip"
+        fi
+      fi
+      if [ "$SERVER" == "dh" ]; then
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          scp out/common/BiTGApps-addon-assistant-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-calculator-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-calendar-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-contacts-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-deskclock-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-dialer-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-gboard-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-markup-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-messages-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-photos-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-soundpicker-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-vanced-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+          scp out/common/BiTGApps-addon-wellbeing-${COMMONADDONRELEASE}_signed.zip ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/addon/non-config
+        fi
+      fi
+    fi
+    if [ -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "sf" ]; then
+        if [ -n "$COMMONADDONRELEASE" ]; then
+          scp out/arm64/BiTGApps-addon-arm64-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-assistant-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-calculator-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-calendar-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-contacts-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-deskclock-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-dialer-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-gboard-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-markup-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-messages-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-photos-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-soundpicker-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-vanced-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+          scp out/common/BiTGApps-addon-wellbeing-${COMMONADDONRELEASE}_unsigned.zip ${user}@${host}:/home/frs/project/bitgapps/addon/non-config
+        fi
+      fi
+    fi
+  fi
+}
+
+# CONFIG sources
+config_sources() {
+  if [ "$CONFIG" == "config" ]; then
+    if [ ! -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "ga" ]; then
+        curl -T addon-config.prop "ftp://${user}:${password}@${host}/config/Addon/addon-config.prop"
+        curl -T cts-config.prop "ftp://${user}:${password}@${host}/config/Safetynet/cts-config.prop"
+        curl -T setup-config.prop "ftp://${user}:${password}@${host}/config/SetupWizard/setup-config.prop"
+      fi
+      if [ "$SERVER" == "dh" ]; then
+        scp addon-config.prop ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/config/Addon
+        scp cts-config.prop ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/config/Safetynet
+        scp setup-config.prop ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/config/SetupWizard
+      fi
+    fi
+    if [ -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "sf" ]; then
+        scp addon-config.prop ${user}@${host}:/home/frs/project/bitgapps/config/Addon
+        scp cts-config.prop ${user}@${host}:/home/frs/project/bitgapps/config/Safetynet
+        scp setup-config.prop ${user}@${host}:/home/frs/project/bitgapps/config/SetupWizard
+      fi
+    fi
+  fi
+}
+
+# APK sources
+apk_sources() {
+  if [ "$APK" == "apk" ]; then
+    if [ ! -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "ga" ]; then
+        if [ -n "$APKRELEASE" ]; then
+          curl -T BiTGApps-v${APKRELEASE}.apk "ftp://${user}:${password}@${host}/APK/BiTGApps-v${APKRELEASE}.apk"
+        fi
+      fi
+      if [ "$SERVER" == "dh" ]; then
+        if [ -n "$APKRELEASE" ]; then
+          scp BiTGApps-v${APKRELEASE}.apk ${user}@${host}:/home/dh_ddbfeb/bitgapps.com/downloads/APK
+        fi
+      fi
+    fi
+    if [ -n "$TESTRELEASE" == "1" ]; then
+      if [ "$SERVER" == "sf" ]; then
+        if [ -n "$APKRELEASE" ]; then
+          scp BiTGApps-v${APKRELEASE}.apk ${user}@${host}:/home/frs/project/bitgapps/apk
+        fi
+      fi
+    fi
+  fi
+}
+
+# Execute functions
+set_credentials
+check_credentials
+arm_sources
+arm64_sources
+common_sources
+config_sources
+apk_sources
